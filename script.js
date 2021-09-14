f852056f6670f4f1d504c9d504cc68a294a95a4c
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-09-14 13:30:43 -0300

    .

---
+++ script.js
@@ -0,0 +1,65 @@
+
+const cards = document.querySelectorAll('.card')
+const dropzones = document.querySelectorAll('.dropzone')
+
+
+cards.forEach(card => {
+    card.addEventListener('dragstart', dragstart)
+    card.addEventListener('drag', drag)
+    card.addEventListener('dragend', dragend)
+})
+
+function dragstart() {
+    console.log('CARD: Start dragging ')
+    dropzones.forEach( dropzone => dropzone.classList.add('highlight'))
+
+    // this = card
+    this.classList.add('is-dragging')
+}
+
+function drag() {
+    console.log('CARD: Is dragging ')
+}
+
+function dragend() {
+    console.log('CARD: Stop drag! ')
+    dropzones.forEach( dropzone => dropzone.classList.remove('highlight'))
+
+    // this = card
+    this.classList.remove('is-dragging')
+}
+
+/** place where we will drop cards */
+dropzones.forEach( dropzone => {
+    dropzone.addEventListener('dragenter', dragenter)
+    dropzone.addEventListener('dragover', dragover)
+    dropzone.addEventListener('dragleave', dragleave)
+    dropzone.addEventListener('drop', drop)
+})
+
+function dragenter() {
+    console.log('DROPZONE: Enter in zone ')
+}
+
+function dragover() {
+    // this = dropzone
+    this.classList.add('over')
+
+    // get dragging card
+    const cardBeingDragged = document.querySelector('.is-dragging')
+
+    // this = dropzone
+    this.appendChild(cardBeingDragged)
+}
+
+function dragleave() {
+    console.log('DROPZONE: Leave ')
+    // this = dropzone
+    this.classList.remove('over')
+
+}
+
+function drop() {
+    console.log('DROPZONE: dropped ')
+    this.classList.remove('over')
+}
